832765d2fa3ca4d3c25a809da64c3d5802612cb4
jankoumich <janko@umich.edu>
2017-04-02 17:48:30 -0700

    add csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 public class Main {
+    /**
+     * TODO:
+     *  read from resources
+     *  user input
+     *  find where / are added to property file
+     *  readme
+     *  tests
+     */
     public static void main(String[] args) {
         System.out.println("Converter Java Properties is starting work");
-        new Converter("/Users/janko/Desktop/test.csv", "/Users/janko/Desktop/test.properties", 1);
+        new Converter("/Users/janko/Desktop/test.csv", "/Users/janko/Desktop/test.properties", 0);
         System.out.println("...and it is done, enjoy :)");
     }
 
