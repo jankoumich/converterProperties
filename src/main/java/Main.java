868c9d42635f4cf8ef0634f73bd1bd3805a7ad8e
jankoumich <janko@umich.edu>
2017-04-03 17:02:16 -0700

    add reading user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     /**
      * TODO:
@@ -7,9 +9,21 @@ public class Main {
      *  tests
      */
     public static void main(String[] args) {
+        System.out.println("Welcome to Converter to Properties");
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a path to your file: ex. /Users/testUser/Desktop/test.csv");
+        String pathToRead = reader.next();
+        System.out.println("Enter a path where to write a file: ex. /Users/testUser/Desktop");
+        String pathToWrite = reader.next();
+        System.out.println("How should we call your new property file? ex. test.properties");
+        String fileName = reader.next();
+        System.out.println("Should we escape special characters with HTML escape sequence? yes -> 1, no -> 0 ");
+        int mode = reader.nextInt();
         System.out.println("Converter Java Properties is starting work");
-        Converter converter =  new Converter("/Users/janko/Desktop/test.csv", "/Users/janko/Desktop/test.properties");
-        converter.convert(0);
+
+        Converter converter =  new Converter(pathToRead, pathToWrite + "/" + fileName);
+
+        converter.convert(mode);
         System.out.println("...and it is done, enjoy :)");
     }
 
