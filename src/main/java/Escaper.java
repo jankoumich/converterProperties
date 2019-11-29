3680a20ecb790016db8fb6df390e8a098dece629
janko <janko@umich.edu>
2019-11-28 21:45:10 -0500

    add jacoco

--- src/main/java/Escaper.java
+++ src/main/java/Escaper.java
@@ -3,7 +3,7 @@
  */
 public class Escaper {
 
-    public String escapeHTML(String s) {
+    public static String escapeHTML(String s) {
         StringBuilder out = new StringBuilder(Math.max(16, s.length()));
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
